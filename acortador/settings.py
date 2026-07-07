af02fb70212c7ae0c23e21090717b7ed02653356
Moisés Valero <moisesvs84@gmail.com>
2026-07-07 19:28:43 +0200

    fix: use /tmp/staticfiles for writable static root on Vercel, run migrations+collectstatic on cold start

--- acortador/settings.py
+++ acortador/settings.py
@@ -89,7 +89,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static" / "dist"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+if os.environ.get("VERCEL"):
+    STATIC_ROOT = "/tmp/staticfiles"
+else:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
--- acortador/wsgi.py
+++ acortador/wsgi.py
@@ -9,5 +9,6 @@ application = get_wsgi_application()
 # En Vercel serverless, ejecuta migrations/collectstatic al arrancar en frío
 if os.environ.get("VERCEL") and os.environ.get("DATABASE_URL"):
     from django.core.management import call_command
+
     call_command("migrate", "--noinput")
-    call_command("collectstatic", "--noinput")
+    call_command("collectstatic", "--noinput", "--ignore=*.map")
