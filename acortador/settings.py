30aba9045069e6b94e55b9eb4e5312110a81f783
Moisés Valero <moisesvs84@gmail.com>
2026-07-07 20:11:20 +0200

    fix: move admin template overrides to acortador/templates and add to TEMPLATES.DIRS for priority

--- acortador/settings.py
+++ acortador/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = "acortador.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "acortador" / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
